6cd7c2f9f1d8008c329789cd51cb5a0eb0397fbd
Margus <marguslanto@gmail.com>
2021-03-21 16:25:04 +0200

    set up basic index.js file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# backend
+# node-backend
---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => res.send(`Hello from port ${port}`));
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
