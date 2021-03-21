2b60bd360a847dd17725453c58780935630072c7
Margus <marguslanto@gmail.com>
2021-03-21 20:45:34 +0200

    remove whitespace

--- server/index.js
+++ server/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
-
 const port = process.env.PORT || 3000;
-
 const app = express();
-
 const apiRoutes = require('./routes/api')
 
 app.use('/api', apiRoutes)
-
 app.get('/', (req, res) => res.send(`Hello from port ${port}`));
 
 app.listen(port, () => console.log(`App is listening on port ${port}`));
