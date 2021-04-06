b137574b1130c5970f8e39b55d4ec95f3ed2ea9c
Margus <marguslanto@gmail.com>
2021-04-06 21:37:50 +0300

    configure eslint

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import apiRoutes from './routes/api';
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use('/api', apiRoutes)
+app.use('/api', apiRoutes);
 app.get('/', (req, res) => res.send(`Hello from port ${port}`));
 
 app.listen(port, () => console.log(`App is listening on port ${port}`));
--- server/routes/api.ts
+++ server/routes/api.ts
@@ -3,10 +3,11 @@ import * as express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('API home page');
-})
+	res.send('API home page');
+});
+
 router.get('/about', (req, res) => {
-  res.send('About API');
-})
+	res.send('About API');
+});
 
 export default router;
